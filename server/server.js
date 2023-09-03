b51a82c9275bd1f5acaf8889a2e9030a0b297ece
MuqarrabG <84062956+MuqarrabG@users.noreply.github.com>
2023-09-03 18:12:46 +1000

    express backend completed

--- server/server.js
+++ server/server.js
@@ -23,6 +23,20 @@ app.get("/api/products/:id", (req, res) => {
   }
 });
 
+app.get("/api/orders", (req, res) => {
+  if (req.query.user_id) {
+    const id = Number(req.query.user_id);
+    const userOrders = data.orders.filter((o) => o.user_id === id);
+
+    if (userOrders.length) {
+      return res.json(userOrders);
+    } else {
+      return res.status(404).send("No orders made by this user");
+    }
+  }
+  res.json(data.orders);
+});
+
 app.get("/api/categories", (req, res) => {
   res.json(data.categories);
 });
@@ -31,25 +45,25 @@ app.get("/api/tags", (req, res) => {
   res.json(data.tags);
 });
 
-app.get("/api/orders", (req, res) => {
-  res.json(data.orders);
-});
+// app.get("/api/orders", (req, res) => {
+//   res.json(data.orders);
+// });
 
 app.get("/api/session", (req, res) => {
   res.json(data.session);
 });
 
-app.get('/api/users/:id', (req, res) => {
-	const id = Number(req.params.id)
-	const user = data.users.filter(u => u.id === id)[0]
-	// return a 404 if there is no such unit
-	if (user) {
-	  res.json(user)
-	} else {
-	  res.status(404)
-	  res.send("<h1>Unit not found.</h1>")
-	}
-})
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = data.users.filter((u) => u.id === id)[0];
+  // return a 404 if there is no such unit
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    res.send("<h1>Unit not found.</h1>");
+  }
+});
 
 app.get("/:universalURL", (req, res) => {
   res.send("404 URL NOT FOUND");
